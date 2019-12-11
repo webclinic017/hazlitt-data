6bf166a09772eef2278038588d5913c79a4db4c9
Alex Younger <alex@izoox.com>
2019-12-10 23:16:02 -0700

    Creating news sync

---
+++ app/Console/Commands/Sync/Headlines.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands\Sync;
+use SimpleXMLElement;
+use App\News;
+use GuzzleHttp\Client;
+
+use Illuminate\Console\Command;
+
+class Headlines extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:name';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        //
+    }
+}
---
+++ app/News.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+
+class News extends Model
+{
+    protected $fillable = [
+        'source',
+        'headline',
+        'date-released',
+    ];
+}
