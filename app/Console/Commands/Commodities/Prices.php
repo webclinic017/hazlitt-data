1d749ceb9bae63eb0bffe86d31d3e56d376fcee2
Alex Younger <alex@izoox.com>
2020-01-10 09:20:08 -0700

    Testing apis

--- app/Console/Commands/Commodities/Prices.php
+++ app/Console/Commands/Commodities/Prices.php
@@ -37,7 +37,21 @@ class Prices extends Command
      * @return mixed
      */
     public function handle()
-    {
-        //
+    {        
+        $url = 'https://api.tradingeconomics.com/markets/commodities';
+        $headers = array(
+            "Accept: application/xml",
+            "Authorization: Client guest:guest"
+        );
+        $handle = curl_init(); 
+            curl_setopt($handle, CURLOPT_URL, $url);
+            curl_setopt($handle, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+        
+            $data = curl_exec($handle);
+        curl_close($handle);
+        //parse your data to satisfy your needs....
+        //showing result
+        $this->info($data); 
     }
 }
--- resources/views/commodities/index.blade.php
+++ resources/views/commodities/index.blade.php
@@ -1,13 +1,17 @@
 @extends('layouts.app')
 @section('content')
-<h1 class="text-2xl text-gray-100">{!! $commodity->name !!}</h1>
-
-<div class="bg-dark-mode-lighter w-1/2 rounded alternating-row">
-    @foreach ($commodity->articles as $article)
-    <div class="row-item p-2">
-        <p class="text-gray-100 text-sm"><a href="{!! $article->url !!}">{!! $article->headline !!}</a></p>
+<div class="container gothic-neo">
+    <h1 class="text-2xl text-gray-100">{!! $commodity->name !!}</h1>
+    <div class="w-1/2">
     </div>
-    @endforeach
+
+    <div class="bg-dark-mode-lighter w-1/2 rounded alternating-row">
+        @foreach ($commodity->articles as $article)
+            <div class="row-item p-2">
+                <p class="text-gray-100 text-sm"><a href="{!! $article->url !!}">{!! $article->headline !!}</a></p>
+            </div>
+        @endforeach
+    </div>
 </div>
 
 @endsection
