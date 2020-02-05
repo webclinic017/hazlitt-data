0f8b81fef1f483fec65e4cbdd691fb90aa33dbb9
Alex Younger <alex@izoox.com>
2020-02-04 21:30:09 -0700

    Spot cron is accurate

--- app/Console/Commands/Commodities/SpotPrices.php
+++ app/Console/Commands/Commodities/SpotPrices.php
@@ -44,39 +44,45 @@ class SpotPrices extends Command
         $start = microtime(true);
         $client = new Client();
         try {
+            $this->comment('Getting...');
             $crawler = $client->request('GET', env('SPOT_URL'));
 
-            $crawler->filter('body')->each(function ($node) {
-                if ($node->filter('td.datatable-item-first')->count() == 0) {
-                    $this->error('Bad selector');
-                    return;
-                }
+            $crawler->filter('body')->each(function ($node) {                
 
-                $node->filter('td.datatable-item-first')->each(function ($item) use ($node) {
-                    $this->comment('getting ' . $item->text() . '...');
+                $node->filter('tr')->each(function ($row) use ($node) {
+                    if ($row->filter('td.datatable-item-first')->count() == 0) {                        
+                        return;
+                    }
+
+                    $item = $row->filter('td.datatable-item-first')->text();
+                    $this->comment('Getting ' . $item . '...');
                     $price_change = '#nch';
                     $day = '#pch';
-                    $week = '#aspnetForm > div.container > div > div.col-lg-8.col-md-9 > div > div:nth-child(3) > table > tbody > tr:nth-child(1) > td:nth-child(6)';
-                    $month = '#aspnetForm > div.container > div > div.col-lg-8.col-md-9 > div > div:nth-child(3) > table > tbody > tr:nth-child(1) > td:nth-child(7)';
-                    $year =  '#aspnetForm > div.container > div > div.col-lg-8.col-md-9 > div > div:nth-child(3) > table > tbody > tr:nth-child(1) > td:nth-child(8)';
-                    $spot = $node->filter('#p')->text();
+                    $week = 'td:nth-child(6)';
+                    $month = 'td:nth-child(7)';
+                    $year =  'td:nth-child(8)';
+                    $spot = $row->filter('#p')->text();
+
+                    if( strpos($spot, ',') !== false ) {
+                        $spot = str_replace(',', null, $spot);
+                    }
                     
                     $change = collect([
-                        'price-change' => $node->filter($price_change)->text(),
-                        'day' => $node->filter($day)->text(),
-                        'weekly' => $node->filter($week)->text(),
-                        'monthly' => $node->filter($month)->text(),
-                        'yearly' => $node->filter($year)->text(),
-                    ]);
-                    
-                    $commodity = Commodity::where('name', '=', $item->text())->first();
+                        'price-change' => (float) $row->filter($price_change)->text(),
+                        'day' => (float) $row->filter($day)->text(),
+                        'weekly' => (float) $row->filter($week)->text(),
+                        'monthly' => (float) $row->filter($month)->text(),
+                        'yearly' => (float) $row->filter($year)->text(),
+                    ]);                    
+
+                    $commodity = Commodity::where('name', '=', $item)->first();
                     if (!$commodity) {
                         return;
                     }
-                    $commodity->spot = $spot;
+                    $commodity->spot = (float) $spot;
                     $commodity->change = $change;
                     $commodity->save();
-                    $this->info($item->text() . ' prices saved.');
+                    $this->info($row->text() . ' - Saved.');
                 });
             });
             // $this->info('Saved ' . $commodity->name . ' spot price');
