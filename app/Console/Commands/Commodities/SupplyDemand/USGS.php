3143db51b385ca5cf9fb72c54ba4edbc1ecbd7e5
Alex Younger <alex@izoox.com>
2020-03-15 01:01:08 -0600

    cleaning up

--- app/Console/Commands/Commodities/SupplyDemand/USGS.php
+++ app/Console/Commands/Commodities/SupplyDemand/USGS.php
@@ -77,26 +77,6 @@ class USGS extends Command
                     foreach ($extensions as $ext) {
                         $this->comment($url . $slug . $ext);
 
-                        // $response = Request::get($url . $slug . $ext);
-                        // $curl = curl_init();
-
-                        // curl_setopt_array($curl, array(
-                        //     CURLOPT_URL => $url . $slug . $ext,
-                        //     CURLOPT_RETURNTRANSFER => true,
-                        //     CURLOPT_ENCODING => "",
-                        //     CURLOPT_TIMEOUT => 30000,
-                        //     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                        //     CURLOPT_CUSTOMREQUEST => "GET",
-                        //     CURLOPT_HTTPHEADER => array(
-                        //         // Set Here Your Requesred Headers
-                        //         'Content-Type: application/csv',
-                        //     ),
-                        // ));
-                        // $response = curl_exec($curl);
-                        // $err = curl_error($curl);
-                        // curl_close($curl);
-                        // fopen($url . $slug . $ext);
-
                         $curl = curl_init();
                         $err = curl_error($curl);
                         curl_setopt($curl, CURLOPT_URL, $url . $slug. $ext);
@@ -107,16 +87,12 @@ class USGS extends Command
                         if ($err) {
                             continue;
                         }
-                        
+
                         $file_path = config('app.downloads_folder') . $year . $commodity->slug . $ext;
                         $file = fopen($file_path, 'w+');
                         fputs($file, $file_data);
                         fclose($file);
-
-                        // sleep(0.5);
-                        // if ($err) {
-                        //     continue;
-                        // }
+                        
                         sleep(2);
                     }
                 }
