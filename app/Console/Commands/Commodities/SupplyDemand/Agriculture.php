f87507dcc67b3b66e1357aa6059d6d8e881839f7
Alex Younger <alex@izoox.com>
2020-03-29 23:16:32 -0600

    Setting up supply demand collector

--- app/Console/Commands/Commodities/SupplyDemand/Agriculture.php
+++ app/Console/Commands/Commodities/SupplyDemand/Agriculture.php
@@ -41,19 +41,35 @@ class Agriculture extends Command
     public function handle()
     {
         $commodities = Commodity::where('sector', '=', 'Agricultural')->get();
-        $directory = 'storage/imports/commodity/agriculture';
+        $directory = 'storage/imports/commodity/supplydemand/agriculture';
         $excel = new SimpleExcel('csv');
 
         $files = scandir($directory);
-        foreach ($files as $file) {    
-            if ($file == "." || $file == ".." || $file == ".DS_Store") { continue; }            
+        foreach ($files as $file) {
+            if ($file == "." || $file == ".." || $file == ".DS_Store") {
+                continue;
+            }
             $excel->parser->loadFile($directory . '/' . $file);
 
+            $headers = $excel->parser->getRow(1);
             $rows = $excel->parser->getField();
-            dd($rows);
-            foreach ($rows as $i => $row) {
-                $i = $i + 1; // Excel sheet starts at 1.
-                // $ticker = $excel->parser->getCell($i, 1);
+            // dd(count($rows));
+
+            $years = [];
+            foreach ($headers as $header) {
+                if ($header == "Variable" || "Unit") {
+                    continue;
+                }
+                array_push($years, $header);
+            }
+
+            foreach ($years as $year) {
+                foreach ($rows as $i => $row) {
+                    $i = $i + 1; // Excel sheet starts at 1.
+                    $year =
+                $variable = $excel->parser->getCell($i, 1);
+                    $unit = $excel->parser->getCell($i, 2);
+                    // $ticker = $excel->parser->getCell($i, 1);
                 // $name = $excel->parser->getCell($i, 2);
 
                 // $stock = new Stock();
@@ -61,6 +77,7 @@ class Agriculture extends Command
                 // $stock->ticker = $ticker;
                 // $stock->index_id = $index->id;
                 // $stock->save();
+                }
             }
         }
     }
