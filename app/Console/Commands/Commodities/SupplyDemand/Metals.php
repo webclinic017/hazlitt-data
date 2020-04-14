8f61bd835d3f5c406d5a60536f8a64f32c362310
Alex Younger <alex@izoox.com>
2020-04-14 00:00:12 -0600

    Agriculture and Metals Supply and demand data scraped

--- app/Console/Commands/Commodities/SupplyDemand/Metals.php
+++ app/Console/Commands/Commodities/SupplyDemand/Metals.php
@@ -40,18 +40,18 @@ class Metals extends Command
      */
     public function handle()
     {
-        function formatArray($array, $years)
+        function formatArray($array, $years, $name)
         {
             foreach ($array as $item) {
                 mb_convert_encoding($item, 'UTF-8', 'UTF-8');
             }
-            if (!empty($array['Snippet'])) {
-
-            } else {
-                $formattedArray = $array->only('Variable', 'Snippet', 'Unit');
-                $yearlyData = $array->splice(3);
+            if (empty($array['Snippet'])) {              
+                $formattedArray = $array->only('Variable', 'Unit');
+                $yearlyData = $array->splice(3);                
                 $timeTable = collect($years)->combine($yearlyData);
                 $formattedArray['years'] = $timeTable;
+            } else {
+                $formattedArray = $array->only('Variable', 'Snippet');
             }
 
             return $formattedArray;
@@ -73,10 +73,8 @@ class Metals extends Command
                 $excel->parser->loadFile($directory . '/' . $file);
 
                 $rows = $excel->parser->getField();
-                $headers = $excel->parser->getRow(1);
-                $years = array_slice($headers, 3);
-                // dd($headers);
-                // dd($years);
+                $headers = $excel->parser->getRow(1);                
+                $years = array_slice($headers, 3);                
 
                 //Values
                 foreach ($rows as $x => $row) {
@@ -87,13 +85,14 @@ class Metals extends Command
                     $fields[$x] = $excel->parser->getRow($x);
                 }
                 //Merging arrays
-                foreach ($fields as $i => $cells) {
+                foreach ($fields as $i => $cells) {                 
                     $data[$i] = collect($headers)->combine(collect($cells));
                 }
 
                 //Formatting
                 foreach ($data as $i => $array) {
-                    $dataFormatted[$i] = formatArray($array, $years);
+                    $name = $commodity->name;
+                    $dataFormatted[$i] = formatArray($array, $years, $name);
                 }
 
                 //Cleaning up
