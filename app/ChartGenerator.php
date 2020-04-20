d862d24aab0589246b98289933044c8b6dbcc0a4
Alex Younger <alex@izoox.com>
2020-04-19 21:52:17 -0600

    Chart generator model updates

--- app/ChartGenerator.php
+++ app/ChartGenerator.php
@@ -21,22 +21,24 @@ class ChartGenerator extends Model
             $randomEvent = rand(1, 100);
             $allTimeHigh = count($prices) > 1 ? max($prices) : 0;
             $lastPrice = (count($prices) > 1 && $i > 1) ? $prices[$i - 1] : null;
-            $percentChange = ($prices != 0 && $lastPrice != 0) ? ($price - $lastPrice) / $lastPrice : 0;            
+            $percentChange = ($prices != 0 && $lastPrice != 0) ? ($price - $lastPrice) / $lastPrice : 0;
+            
+            //Improbable Events
+            if ($randomEvent == 1) {
+                $price = $price - 10;
+                array_push($prices, $price);
+                continue;
+            }
+            if ($randomEvent == 100) {
+                $price = $price + 8;
+                array_push($prices, $price);
+                continue;
+            }
             
             if ($randomEvent > 50) {                
                 //Buy side variables
                 if ($randomEvent >= 85 && $randomEvent < 100) {
                     $price = $price + 5;
-                    array_push($prices, $price);
-                    continue;
-                } elseif (($lastPrice) && ($percentChange < -0.07)) {
-                    $price = $price + 3;
-                    array_push($prices, $price);
-                    continue;
-                } elseif (($lastPrice) && ($percentChange < -0.15)) {
-                    $price = $price + 5;
-                    array_push($prices, $price);
-                    continue;
                 } else {
                     $price = $price + 1;
                 }
@@ -44,44 +46,13 @@ class ChartGenerator extends Model
                 // Sell side variables
                 if ($randomEvent > 1 && $randomEvent <= 16) {
                     $price = $price - 5;
-                    array_push($prices, $price);
-                    continue;
-                } elseif (($lastPrice) && ($percentChange > 0.07)) {
-                    $price = $price - 3;
-                    array_push($prices, $price);
-                    continue;
-                } elseif (($lastPrice) && ($percentChange > 0.15)) {
-                    $price = $price - 5;
-                    array_push($prices, $price);
-                    continue;
                 } else {
                     $price = $price - 1;                
                 }
             }
 
-            //Long side variables
-            if (($allTimeHigh > 52) && ($price - $allTimeHigh)/$allTimeHigh < -0.5) {
-                $price = $price + 10;
-                array_push($prices, $price);
-                continue;
-            }
-            if (($allTimeHigh > 52) && ($price - $allTimeHigh)/$allTimeHigh > 0.5) {
-                $price = $price - 10;
-                array_push($prices, $price);
-                continue;
-            }
-            if ($price < 0) {
-                $price = 0;
-            }
-
-            //Improbable Events
-            switch ($randomEvent) {
-                case 1:
-                    $price = $price + 30;
-                break;
-                case 100:
-                    $price = $price - 50;
-                break;
+            if ($price < 5) {
+                $price = 5;
             }
             
 
